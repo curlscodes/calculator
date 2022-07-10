55b07f5564438774ba77f6b2cc6ceb1cf3d75788
curlscodes <brickleylewis@gmail.com>
2022-07-10 20:56:24 +0100

    calculator script works with basic inputs (2 numbers, one operator)

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const screen = document.getElementById('screen')
 const numButtons = document.getElementsByClassName('btnNum')
 const opButtons = document.getElementsByClassName('btnOp')
+const equalsButton = document.getElementById('btnEquals')
+equalsButton.addEventListener('click', () => equals())
+const clearButton = document.getElementById('btnClear')
+clearButton.addEventListener('click', () => clear())
 
 function add(a, b) {return a + b}
 function subtract(a, b) {return a - b}
@@ -20,8 +24,22 @@ function operate(a, op, b) {
     }
 }
 
-function appendNum(number) {
-    screen.textContent += number
+function equals() {
+    let opList = screen.textContent.split(' ')
+    let a = Number(opList[0])
+    let b = Number(opList[2])
+    let result = operate(a, opList[1], b)
+    screen.textContent = result
+}
+
+function clear() {
+    screen.textContent = ''
+}
+
+function appendNum(input) {
+    if (input == '*' || input == '+' || input == '-' || input == '+') {
+        screen.textContent += ` ${input} `
+    } else screen.textContent += input
 } 
 
 // adds event listeners to number buttons
